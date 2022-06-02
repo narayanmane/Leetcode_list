3f6951549108560e27c1cc8c72c30d114b428f94
narayanmane <88097660+narayanmane@users.noreply.github.com>
2022-06-02 09:11:08 +0530

    Create 572. Subtree of Another Tree.cpp

---
+++ 572. Subtree of Another Tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool ans=false;
+    bool match(TreeNode* root, TreeNode* subroot)
+    {
+        if(root!=NULL && subroot!=NULL)
+        {
+            bool a= match(root->left,subroot->left);
+            bool b= match(root->right,subroot->right);
+            if(root->val == subroot->val && a && b)
+                return true;
+            else
+                return false;
+        }
+        else if (root==NULL && subroot == NULL)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    void inorder(TreeNode* root, TreeNode* subroot){
+        if(root!=NULL)
+        {
+            inorder(root->left,subroot);
+            bool x=match(root,subroot);
+            if(x) 
+                ans=x;
+            inorder(root->right,subroot);
+        }
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        inorder(root,subRoot);
+        return ans;
+        
+    }
+};
